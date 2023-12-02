6c6de16e84bd58230a6bfff9c4f1e51b8a5d4d04
marianovungel <vungem88@gmail.com>
2023-12-02 13:39:08 -0300

    Tema models

--- models/Tema.js
+++ models/Tema.js
@@ -4,7 +4,7 @@ const TemaSchema = new mongoose.Schema({
     title:String,
     img:String,
     obj:String,
-    status:String
+    status:{ type: Boolean, default: false }
 },
     {timestamps: true}
 );
